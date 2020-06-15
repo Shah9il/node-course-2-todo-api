d4438fc131743ee63fc675eb446b6c1ab185413e
Mirza Golam Abbas Shahneel <mgashahneel@gmail.com>
2020-06-15 23:33:19 +0600

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -24,12 +25,31 @@ app.get('/todos',(req,res)=>{
     Todo.find().then((todos)=>{
         res.send({
             todos,
+            result: 'SUCCESS',
             totalTodos: todos.length
         });
     },(e)=>{
         res.status(400).send(e);
     });
 });
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    };
+    Todo.findById(id).then((todo)=>{
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.status(200).send({
+            todo,
+            result: 'SUCCESS'
+        });
+    },(e)=>{
+        res.status(400).send({});
+    });
+})
 
 app.listen(3000,()=>{
     console.log(`Started on port 3000`);
