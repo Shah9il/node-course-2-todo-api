1d20805eaae5e2432f9e7af8ca6973648214a72d
Mirza Golam Abbas Shahneel <mgashahneel@gmail.com>
2020-06-25 03:23:28 +0600

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,23 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+var password = '1234abc!';
+
+bcrypt.genSalt(10,(err,salt)=>{
+    bcrypt.hash(password,salt,(err,hash)=>{
+        console.log(hash);
+    });
+});
+
+var hashedPassword = '$2a$10$kukcTdo3rEva4JdGTdGHEOcg7A.T3mxSP/mC4aQkLwUPYUXPrskZ6';
+bcrypt.compare(password,hashedPassword,(err,res)=>{
+    console.log(res);
+});
+
+
+
+/* 
 var data = {
     id: 10
 };
@@ -7,7 +25,7 @@ var token = jwt.sign(data,'123abc');
 console.log(token);
 
 var decoded = jwt.verify(token,'123abc');
-console.log(decoded);
+console.log(decoded); */
 
 /* var message = 'I am user number 3';
 var hash = SHA256(message).toString();
